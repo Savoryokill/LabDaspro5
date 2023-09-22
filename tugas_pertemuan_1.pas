f1909f5e233aeae90616ba643d0e8a13a1afe0a3
savoryokill <annisaputriaprilia12@gmail.com>
2023-09-22 09:41:25 +0700

    kuis 1

---
+++ tugas_pertemuan_1.pas
@@ -0,0 +1,17 @@
+uses wincrt;
+var
+nama,kom,alamat:string;
+nim:integer;
+begin
+write('Maskkan nama anda :');
+readln(nama);
+write('Maskkan nim anda :');
+readln(nim);
+write('Maskkan kom anda :');
+readln(kom);
+write('Maskkan alamat anda :');
+readln(alamat);
+write('Hallo, nama saya ',nama,', Nim saya ',nim,', saya di kom ',kom,', alamat saya di ',alamat,', salam kenal semua!')
+
+
+end.
